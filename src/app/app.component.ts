ca8a700eea2206f82d46138615870594190c6c58
arthur <a.lannelucq@gmail.com>
2023-08-05 10:54:02 +0200

    :tada: Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatSnackBarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reusable-form-component-app';
+  form = inject(FormBuilder).group({
+    firstname: ['', [Validators.required]],
+    lastname: ['', [Validators.required]],
+    company: ['', [Validators.required]],
+    email: ['', [Validators.email]],
+    phone: ['',],
+    job: ['', [Validators.required]],
+  });
+
+  addContact() {
+    this.form.markAllAsTouched();
+    if (!this.form.valid) return;
+    console.log(`Inscription réussie ! 🎉`)
+  }
 }
